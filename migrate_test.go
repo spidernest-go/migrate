10b67c37ed4f25c9e6c62b24a1b3a389e2c208de
Evan Rettman <erettman@masonlive.gmu.edu>
2019-06-02 10:20:05 -0400

    started TestLast

--- migrate_test.go
+++ migrate_test.go
@@ -26,6 +26,7 @@ func TestMain(m *testing.M) {
 	if err != nil {
 		panic(err)
 	}
+	tableName = "__meta"
 	defer Builder.Close()
 	os.Exit(m.Run())
 }
@@ -39,3 +40,20 @@ func TestApply(t *testing.T) {
 	assert.NoError(t, Apply(0, "some name", TestEntry, Builder))
 }
 
+func TestLast(t *testing.T) {
+	clear()
+	migrationName := "some name"
+	migrationVer := uint8(0)
+
+	lastMigration, err := Last(Builder)
+	assert.Nil(t, lastMigration, "If there are no migrations the result should be nil for the returned migration.")
+	assert.NoError(t, err, "No error should occur when the table is queryed for a migration if none exist.")
+	assert.NoError(t, Apply(migrationVer, migrationName, TestEntry, Builder), "We expected apply to work but it did not.")
+
+	lastMigration, err = Last(Builder)
+	if assert.NotNil(t, lastMigration, "We expected that a migration would be return and it did not get returned.") {
+		assert.Equal(t, lastMigration.Name, migrationName, "We expected the migration name in the meta table to be the same as the one we tried to apply.")
+		assert.Equal(t, lastMigration.Name, migrationVer, "We expected the migration version in the meta table to be the same as the one we tried to apply.")
+	}
+	assert.NoError(t, err, "We expected no error retrieving the last migration and we expected one to exist since we just added one.")
+}
