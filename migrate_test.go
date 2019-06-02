47ecf3af449fe069bd495010102c9ae130076966
Evan Rettman <erettman@masonlive.gmu.edu>
2019-06-02 10:19:53 -0400

    started TestApply

--- migrate_test.go
+++ migrate_test.go
@@ -1,11 +1,13 @@
 package migrate
 
 import (
+	"bytes"
 	"os"
 	"testing"
 
 	"github.com/spidernest-go/db/lib/sqlbuilder"
 	"github.com/spidernest-go/db/mysql"
+	"github.com/stretchr/testify/assert"
 )
 
 var Builder sqlbuilder.Database
@@ -16,11 +18,24 @@ var Settings = mysql.ConnectionURL{
 	Password: os.Getenv("MYSQL_PASS"),
 }
 
+var TestEntry = bytes.NewBufferString("UPDATE * FROM users WHERE id = 1;")
+
 func TestMain(m *testing.M) {
 	var err error
 	Builder, err = mysql.Open(Settings)
 	if err != nil {
 		panic(err)
 	}
+	defer Builder.Close()
 	os.Exit(m.Run())
 }
+
+func clear() {
+	Builder.Exec("DROP TABLE __meta")
+}
+
+func TestApply(t *testing.T) {
+	clear()
+	assert.NoError(t, Apply(0, "some name", TestEntry, Builder))
+}
+
