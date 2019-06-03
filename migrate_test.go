6c9c314517f62b24104bf320f02184a10e8d4e89
Evan Rettman <erettman@masonlive.gmu.edu>
2019-06-03 01:03:29 -0400

    TestLast updated

--- migrate_test.go
+++ migrate_test.go
@@ -63,18 +63,22 @@ func TestApply(t *testing.T) {
 
 func TestLast(t *testing.T) {
 	clear()
-	migrationName := "some name"
+	migrationName := MigrationName[0]
 	migrationVer := uint8(0)
 
 	lastMigration, err := Last(Builder)
 	assert.Nil(t, lastMigration, "If there are no migrations the result should be nil for the returned migration.")
 	assert.NoError(t, err, "No error should occur when the table is queryed for a migration if none exist.")
-	assert.NoError(t, Apply(migrationVer, migrationName, TestEntry, Builder), "We expected apply to work but it did not.")
+
+	// Put something in the __meta database to play with
+	assert.NoError(t, Apply(0, migrationName, GoodEntry[0], Builder), "We expected apply to work but it did not.")
 
 	lastMigration, err = Last(Builder)
 	if assert.NotNil(t, lastMigration, "We expected that a migration would be return and it did not get returned.") {
-		assert.Equal(t, lastMigration.Name, migrationName, "We expected the migration name in the meta table to be the same as the one we tried to apply.")
-		assert.Equal(t, lastMigration.Name, migrationVer, "We expected the migration version in the meta table to be the same as the one we tried to apply.")
+		assert.NotNil(t, lastMigration.Applied, "We expected the migration application time in the meta table to be something.")
+		assert.Equal(t, lastMigration.Name, migrationName[0], "We expected the migration name in the meta table to be the same as the one we just applied.")
+		assert.Equal(t, lastMigration.Version, migrationVer, "We expected the migration version in the meta table to be the same as the one we just applied.")
 	}
 	assert.NoError(t, err, "We expected no error retrieving the last migration and we expected one to exist since we just added one.")
+
 }
